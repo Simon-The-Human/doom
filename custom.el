a5d6e0d936f3d9773ac6f1ca49e66d19f0bcd076
Simon_The_Human <semirle@ya.ru>
2025-05-15 14:38:50 +0300

    init

---
+++ custom.el
@@ -0,0 +1,50 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(jupyter org-roam-ui pandoc saveplace-pdf-view org-noter-pdftools pdf-tools evil-org eglot pyenv-mode evil-mc evil-multiedit))
+ '(safe-local-variable-values
+   '((python-shell-interpreter . "/ssh:10.92.14.27:~/.venv_paas/bin/python")
+     (ssh-deploy-async-with-threads . 1)
+     (ssh-deploy-async . 1)
+     (ssh-deploy-verbose . 1)
+     (eval add-to-list 'excluded ".dir-locals.el")
+     (eval add-to-list 'excluded ".gitignore")
+     (eval setq excluded
+      '(".git/"))
+     (eval add-to-list 'excluded "~/projects/adsw/projects/paas_cloudru_evolution/.dir-locals.el")
+     (eval add-to-list 'excluded "~/projects/adsw/projects/paas_cloudru_evolution/.gitignore")
+     (eval setq excluded
+      '("~/projects/adsw/projects/paas_cloudru_evolution/.git/"))
+     (ssh-deploy-exclude-list . excluded)
+     (python-shell-interpreter . "~/sshfs/venvs230/venv230/bin/python")
+     (python-shell-interpreter . "/home/simon/sshfs/venvs230/venv230/bin/python")
+     (python-shell-interpreter . "/home/simon/sshfs/venv230/aldpro-common/bin/python")
+     (python-shell-interpreter . "/home/simon/sshfs/aldpro_core_venvs/.venv-230/bin/python")
+     (python-shell-interpreter . "/ssh:pve|ssh:dc-01.ald230:/opt/rbta/venvs/aldpro-common/bin/python")
+     (python-shell-interpreter . "/ssh:root@192.168.122.15:dc-01.ald230:/opt/rbta/venvs/aldpro-common/bin/python")
+     (ssh-deploy-on-explicit-save . 1)
+     (python-shell-interpreter . ".venv/bin/python")
+     (python-shell-interpreter . "/bin/python3")
+     (python-shell-interpreter . "/bin/python")
+     (python-shell-interpreter . ".venv/bin/python3")
+     (python-shell-interpreter . "./.venv/bin/python3")
+     (python-shell-interpreter . "./.venv/bin/python")
+     (flycheck-eglot-mode)
+     (flycheck-eglot-setup)
+     (eval flycheck-eglot-setup)
+     (python-shell-interpreter . "/opt/rbta/venvs/aldpro-common/bin/python")))
+ '(smtpmail-smtp-server "new-mail.astralinux.ru")
+ '(smtpmail-smtp-service 25))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'scroll-left 'disabled nil)
