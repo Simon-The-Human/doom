c653296101a558df9af94f562d42c7af286796cd
Simon_The_Human <semirle@ya.ru>
2025-06-05 15:36:53 +0300

    added pdf roll mode

--- packages.el
+++ packages.el
@@ -53,3 +53,16 @@
 (package! org-roam-ui)
 (package! protobuf-mode :recipe (:nonrecursive t))
 (package! hcl-mode)
+(package! pdf-tools :recipe
+          (:host github
+                 :repo "dalanicolai/pdf-tools"
+                 :branch "pdf-roll"
+                 :files ("lisp/*.el"
+                         "README"
+                         ("build" "Makefile")
+                         ("build" "server")
+                         (:exclude "lisp/tablist.el" "lisp/tablist-filter.el"))))
+
+(package! image-roll :recipe
+          (:host github
+                 :repo "dalanicolai/image-roll.el"))
