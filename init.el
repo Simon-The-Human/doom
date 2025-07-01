13a84a6e9b495c2c987e415c524de959e1f5e0ba
Simon_The_Human <semirle@ya.ru>
2025-07-01 14:46:22 +0300

    added rust; format extended

--- init.el
+++ init.el
@@ -58,7 +58,9 @@
         +everywhere)    ; come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format          ; automated prettiness
+        +onsave
+        +eglot)
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
@@ -173,15 +175,14 @@
        (python
         +lsp
         +poetry
-        +pyenv)
-                      ; beautiful is better than ugly
+        +pyenv)          ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        (scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
